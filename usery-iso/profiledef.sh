6d83f0cc151a0b86a2a3ccc4e541e74aa2654f98
Usery Builder <usery@example.com>
2026-06-06 23:24:54 +0530

    feat: pivot to alci base, customize branded wavy wallpaper, add dual waybar with bottom dock, and resolve hyprland config errors

--- usery-iso/profiledef.sh
+++ usery-iso/profiledef.sh
@@ -2,23 +2,34 @@
 # shellcheck disable=SC2034
 
 iso_name="usery"
-iso_label="USERY_$(date +%Y%m)"
-iso_publisher="Usery Project"
+iso_label="USERY_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="Usery OS Team"
 iso_application="Usery OS Live/Rescue System"
-iso_version="$(date +%Y.%m.%d)"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux' 'uefi.systemd-boot')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.systemd-boot.esp' 'uefi-x64.systemd-boot.esp'
+           'uefi-ia32.systemd-boot.eltorito' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
-
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
-  ["/etc/shadow"]="0:0:0400"
-  ["/etc/gshadow"]="0:0:0400"
-  ["/etc/passwd"]="0:0:0644"
-  ["/etc/group"]="0:0:0644"
-  ["/root"]="0:0:0750"
-  ["/root/.automated_script.sh"]="0:0:0755"
-  ["/root/.gnupg"]="0:0:0700"
-  ["/usr/local/bin/choose-mirror"]="0:0:0755"
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/root/.gnupg"]="0:0:700"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/etc/polkit-1/rules.d"]="0:0:750"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/usr/local/bin/alci-make-a-pure-arch"]="0:0:755"
+  ["/usr/local/bin/alci-displaymanager-check"]="0:0:755"
+  ["/usr/local/bin/alci-virtual-machine-check"]="0:0:755"
+  ["/usr/local/bin/get-nemesis-on-alci"]="0:0:755"
+  ["/usr/local/bin/fix-keys"]="0:0:755"
+  ["/usr/local/bin/get-chadwm"]="0:0:755"
 )
